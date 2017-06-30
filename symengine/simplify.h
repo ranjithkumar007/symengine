535115f8d9deb9c8e3a840024a7f14e573cb1e8f
ranjithkumar007 <ranjith.dakshana2015@gmail.com>
2017-07-01 01:09:04 +0530

    update

--- symengine/simplify.h
+++ symengine/simplify.h
@@ -9,45 +9,46 @@
 
 namespace SymEngine
 {
-	RCP<const Basic> bottom_up(const RCP<const Basic>&rv, const std::function<RCP<const Basic>> &f)
-	{
-		if (is_a<Sign>(*rv) or is_a<Erf>(*rv) or is_a<Erfc>(*rv)
-	         or is_a<Gamma>(*rv) or is_a<LogGamma>(*rv) or is_a<Sin>(*rv)
-	         or is_a<Cos>(*rv) or is_a<Tan>(*rv) or is_a<Cot>(*rv)
-	         or is_a<Sec>(*rv) or is_a<Csc>(*rv) or is_a<Sinh>(*rv)
-	         or is_a<Cosh>(*rv) or is_a<Tanh>(*rv) or is_a<Coth>(*rv)
-	         or is_a<Sech>(*rv) or is_a<Csch>(*rv)) {
-	         const OneArgFunction &dfunc = down_cast<const OneArgFunction &>(*rv);
-	     	 auto farg = dfunc.get_arg();
-	     	 auto newarg = bottom_up(farg, f);
-	     	 auto nrv = rv; 
-	     	 if (not eq(*newarg, *farg))
-	     	 	nrv = dfunc.create(newarg);
-	         return f(nrv);
-	     }
-	     if (is_a<ATan2>(*rv) or is_a<LowerGamma>(*rv) or is_a<UpperGamma>(*rv)
-	         or is_a<Beta>(*rv)) {
-	         const TwoArgFunction &dfunc = down_cast<const TwoArgFunction &>(*rv);
-	     	 auto farg1 = dfunc.get_arg1(), farg2 = dfunc.get_arg2();
-	     	 auto newarg1 = bottom_up(farg1, f), newarg2 = bottom_up(farg2, f);
-	     	 auto nrv = rv;
-	     	 if (farg1 != newarg1 or farg2 != newarg2)
-	     	 	nrv = dfunc.create(newarg1, newarg2);
-	         return f(nrv);
-	     }
-	     if (is_a<LeviCivita>(*rv) or is_a<Max>(*rv) or is_a<Min>(*rv)) {
-	     	const MultiArgFunction &dfunc = down_cast<const MultiArgFunction &>(*rv);
-	        auto fargs = dfunc.get_args();
-	        vec_basic newargs;
-	        for (const auto & a : fargs) {
-	        	newargs.push_back(bottom_up(a, f));
-	        } 
-	        auto nrv = rv;
-	        if (not eq(*fargs, *newargs)) {
-	        	nrv = dfunc.create(nrv);
-	        }
-	        return f(nrv);	
-	     }
-	     return rv;
-	}
+RCP<const Basic> bottom_up(const RCP<const Basic> &rv,
+                           const std::function<RCP<const Basic>> &f)
+{
+    if (is_a<Sign>(*rv) or is_a<Erf>(*rv) or is_a<Erfc>(*rv) or is_a<Gamma>(*rv)
+        or is_a<LogGamma>(*rv) or is_a<Sin>(*rv) or is_a<Cos>(*rv)
+        or is_a<Tan>(*rv) or is_a<Cot>(*rv) or is_a<Sec>(*rv) or is_a<Csc>(*rv)
+        or is_a<Sinh>(*rv) or is_a<Cosh>(*rv) or is_a<Tanh>(*rv)
+        or is_a<Coth>(*rv) or is_a<Sech>(*rv) or is_a<Csch>(*rv)) {
+        const OneArgFunction &dfunc = down_cast<const OneArgFunction &>(*rv);
+        auto farg = dfunc.get_arg();
+        auto newarg = bottom_up(farg, f);
+        auto nrv = rv;
+        if (not eq(*newarg, *farg))
+            nrv = dfunc.create(newarg);
+        return f(nrv);
+    }
+    if (is_a<ATan2>(*rv) or is_a<LowerGamma>(*rv) or is_a<UpperGamma>(*rv)
+        or is_a<Beta>(*rv)) {
+        const TwoArgFunction &dfunc = down_cast<const TwoArgFunction &>(*rv);
+        auto farg1 = dfunc.get_arg1(), farg2 = dfunc.get_arg2();
+        auto newarg1 = bottom_up(farg1, f), newarg2 = bottom_up(farg2, f);
+        auto nrv = rv;
+        if (farg1 != newarg1 or farg2 != newarg2)
+            nrv = dfunc.create(newarg1, newarg2);
+        return f(nrv);
+    }
+    if (is_a<LeviCivita>(*rv) or is_a<Max>(*rv) or is_a<Min>(*rv)) {
+        const MultiArgFunction &dfunc
+            = down_cast<const MultiArgFunction &>(*rv);
+        auto fargs = dfunc.get_args();
+        vec_basic newargs;
+        for (const auto &a : fargs) {
+            newargs.push_back(bottom_up(a, f));
+        }
+        auto nrv = rv;
+        if (not eq(*fargs, *newargs)) {
+            nrv = dfunc.create(nrv);
+        }
+        return f(nrv);
+    }
+    return rv;
+}
 };
